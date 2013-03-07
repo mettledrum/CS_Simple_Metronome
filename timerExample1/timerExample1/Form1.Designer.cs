e8ac95446531f209b5ea08bbebc90809e3b20c75
Andrew <mettledrum@gmail.com>
2013-03-06 22:30:58 -0700

    first attempt at VB project upload
    probably has way too much shit in it

---
+++ timerExample1/timerExample1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace timerExample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "120";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(137, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "TAP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 59);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "meter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BPM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(262, 121);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Simple Metronome";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ timerExample1/timerExample1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//for stopwatch()
+using System.Diagnostics;
+
+//fo sounds
+using System.Media;
+
+// 3/3/13, 3/4, 3/5
+//A first attempt at making a metronome form for windows.
+// To use: Stopwatch() for getting the tempo, then a Timer()
+// stuffed with the Stopwatch interval for making events
+// that show or sound some blips.
+
+//Then, make some time signature allowances and use modulus
+// for some simple downbeat blips that are different from
+// the filler bleeps.
+
+//numericUpDown - rounds up or down based on decimal entered,
+// but the float to int always rounds down, so there's a 
+// discrepancy IF a decimal that rounds up is typed.
+//just limit it to ReadOnly=true so user can't mess it up.
+
+
+namespace timerExample1
+{
+    public partial class Form1 : Form
+    {
+        //keeps track of the time
+        private Timer timer = new Timer();
+
+        //for TAP button, sets BPM
+        private Stopwatch s1 = new Stopwatch();
+
+        //sounds to load, upbeats, downbeats
+        private SoundPlayer DBsound = new SoundPlayer(Properties.Resources.cl1);
+        private SoundPlayer UBsound = new SoundPlayer(Properties.Resources.cl2);
+
+        //meter variables
+        private int Tsign;
+        private int TcountDown;
+
+        public Form1()
+        {
+            //some sort of beginning stuff for form I think?
+            InitializeComponent();
+
+            //user can only use arrows
+            numericUpDown1.ReadOnly = true;
+
+            //can't resize the form
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            //timer init
+            timer.Tick += new EventHandler(timer_Tick);     //every time timer ticks, timer_Tick is called
+            timer.Interval = 500;                           //default 120 BPM
+            
+            //show BPM on label1
+            int BPM = (int)(60000 / timer.Interval);
+            label1.Text = BPM.ToString();
+
+            //default time signature is 1
+            Tsign = (int)numericUpDown1.Value;
+            TcountDown = Tsign;
+        }
+
+        //subscribed to t1.Tick event
+        void timer_Tick(object sender, EventArgs e)
+        {
+            //downbeat or not based in time sig. vars
+            if (TcountDown == 1)
+            {
+                DBsound.Play();
+                TcountDown = Tsign;
+            }
+            else
+            {
+                UBsound.Play();
+                --TcountDown;
+            }
+        }
+
+        //starts timer
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timer.Enabled == false)
+            {
+                button1.Text = "STOP";
+                timer.Enabled = true;
+                timer.Start();                              //Start the timer
+            }
+            else
+            {
+                button1.Text = "START";
+                timer.Enabled = false;
+                timer.Stop();
+            }
+        }
+
+        //TAP button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //default already set to 120 on constructor
+            if(s1.ElapsedMilliseconds != 0)
+            {
+                //only get interval after second click of TAP
+                timer.Interval = (int)(s1.ElapsedMilliseconds);     //cannot take 0
+            }
+
+            //starts the stopwatch over again, AFTER getting the interval value
+            s1.Restart();
+
+            //show BPM on label1
+            int BPM = (int)(60000 / timer.Interval);
+            label1.Text = BPM.ToString();
+        }
+
+        //time signature value changed
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Tsign = (int)numericUpDown1.Value;
+            TcountDown = Tsign;
+        }
+    }
+}
